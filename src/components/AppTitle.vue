82f8de7930bc341865551377858d3867af0169cc
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-04-12 13:42:08 +0200

    add files

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+	<header>
+		<div>
+			<h1 class="d-flex flex-column align-items-center justify-copntent-center">{{ title }}</h1>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "appNav",
+	data() {
+		return {
+			title: "hello world",
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
